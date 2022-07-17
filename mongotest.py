f8372061f46d4f7d8add0ca53026075ab540a25a
Tilak <https://github.com/Tilak1309/PythonGIT.git>
2022-07-17 20:56:25 +0530

    this is my 1st commit to GIT

--- mongotest.py
+++ mongotest.py
@@ -1,6 +1,8 @@
+# import ssl
+# ssl_cert_reqs=ssl.CERT_NONE
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://mongodb:mongodb@cluster0.4pk5c.mongodb.net/?retryWrites=true&w=majority")
-# client = pymongo.MongoClient("mongodb://test:mongodb@cluster0-shard-00-00.vye4g.mongodb.net:27017,cluster0-shard-00-01.vye4g.mongodb.net:27017,cluster0-shard-00-02.vye4g.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-9d2upr-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://mongodb:mongodb@cluster0.4pk5c.mongodb.net/?retryWrites=true&w=majority",tls=True,tlsAllowInvalidCertificates=True)
+
 db = client.test
 print(db)
 
